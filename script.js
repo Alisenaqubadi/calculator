188c23a6b771d32eec6bbdbd45b70aeaa81a20c2
Alisenaqubadi <131773898+Alisenaqubadi@users.noreply.github.com>
2025-03-26 15:51:45 +0500

    calculator completed

---
+++ script.js
@@ -0,0 +1,28 @@
+let Numbers = "";
+let result;
+
+function operator(op) {
+    if(Numbers !== "" && !Numbers.endsWith('* ') && !Numbers.endsWith('- ') && !Numbers.endsWith('+ ') && !Numbers.endsWith('/ ')){
+    Numbers = Numbers + " " + op + " ";
+    document.querySelector(".display").innerHTML = `<h1>${Numbers}</h1>`;
+    }
+};
+
+function number(Num) {
+    Numbers = Numbers + Num;
+    console.log(Numbers);
+    document.querySelector(".display").innerHTML = `<h1>${Numbers}</h1>`;
+};
+
+function clearDisplay() {
+    Numbers = "";
+    document.querySelector('.display').innerHTML = "<h1>0</h1>"
+};
+
+function equal() {
+    function newEval (a){
+        return new Function("return " + a)();
+    }
+    result = newEval(Numbers);
+    document.querySelector('.display').innerHTML = `<h1>${result}</h1>`;
+};
